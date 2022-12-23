5d9a04fb096832bcd2dfce318a0a598f3098ac60
Ayan Ruzdan <ayanruzdan@gmail.com>
2022-12-23 18:25:31 +0530

    README.md UPDATED

--- Prototype-2.py
+++ Prototype-2.py
@@ -1,6 +1,7 @@
 from tkinter import *
 root = Tk()
 root.title("GCity Railways")
-root.geometry("800x800")
-exit_button = Button(root, text="Exit", command=root.destroy).place(x=0, y=0)
+root.geometry("400x300")
+header = Label(root, text="GCity Railways").pack()
+exit_button = Button(root, text="Exit", command=root.destroy).place(x=180, y=130)
 root.mainloop()
